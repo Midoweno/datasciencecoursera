5b4ec46adb873f1a66d2f641bbef15efcd549b6e
Reno <reguo@ucsd.edu>
2026-01-26 20:02:11 -0800

    3rd try lol

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,1 +1,9 @@
+library(readr)
+library(readxl)
 
+##This is the Final Script.
+directory<-"C:/Users/Tower 200/Downloads/rprog_data_specdata/specdata"
+
+pollutantmean<-function(directory,pollutant,id=1:332){
+  read.csv()
+}
